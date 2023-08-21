af13be6e56785023d16900cb481de22c2e642800
Trinity <111662820+TrinityDN@users.noreply.github.com>
2023-08-21 19:07:09 -0300

    Update cadastroJS.js

--- cadastroJS.js
+++ cadastroJS.js
@@ -10,6 +10,7 @@ function categoria2_cadastroinfo() {
       div.style.width = "60%";
     });
 
+    console.log(valorSelect1_cadastroInfo)
     //para deixar a segunda categoria de escolha invisível
     var categorias2 = document.querySelectorAll(".categoriaSelecionada_cadastroInfo"
     );
@@ -23,6 +24,8 @@ function categoria2_cadastroinfo() {
       document.getElementById("opcaovazia_cadastroInfo").style.display = 'none';
       document.querySelector("#escolha_" + valorSelect1_cadastroInfo + "_cadastroInfo").style.display = "flex";
       document.querySelector("#div_cadastroInfo_" + valorSelect1_cadastroInfo).style.display = "block";
+
+      console.log("#div_cadastroInfo_" + valorSelect1_cadastroInfo)
     }
     else if (valorSelect1_cadastroInfo === "professor") {
       document.querySelector("#div_cadastroInfo_" + valorSelect1_cadastroInfo).style.display = "block";
@@ -47,7 +50,6 @@ function addDisciplina(){
   const materias = ["Matéria I", "Matéria II", "Matéria III"];
 
   //Criação de elementos para adicionar ao corpo da página
-  const corpo = document.body;
   const deletar = document.createElement('button');
   const imgDel = document.createElement('img');
   const div = document.createElement('div');
@@ -55,19 +57,60 @@ function addDisciplina(){
   const texto = document.createTextNode(materias[0]);
   imgDel.src = "img/sinal-de-menos.png";
 
+  const resposta1 = document.querySelector("#resposta");
+
   //Identificando uma div para estilizar
-  div.id = "alinhamento"
+  div.classList.add("alinhamento");
+  deletar.classList.add("btnDel");
 
   //Contrapondo elementos
   resultado.appendChild(texto)
   deletar.appendChild(imgDel);
   div.appendChild(deletar);
   div.appendChild(resultado);
-  corpo.appendChild(div);
+  resposta1.appendChild(div);
 
   //Evento para remover disciplina adicionada anteriormente
   deletar.addEventListener("click", () => {
     resultado.remove();
     deletar.remove();
   });
+}
+
+//Função para adicionar professores na área de TURMA
+function addProfessor(){
+  //Array futuro para cadastramento de matérias
+  const professores = ["Professor I", "Professor II", "Professor III"];
+
+  //Criação de elementos para adicionar ao corpo da página
+  const deletar = document.createElement('button');
+  const imgDel = document.createElement('img');
+  const div = document.createElement('div');
+  const resultado = document.createElement('p');
+  const texto = document.createTextNode(professores[0]);
+  imgDel.src = "img/sinal-de-menos.png";
+
+  const resposta1 = document.querySelector("#respostaDois");
+
+  //Identificando uma div para estilizar
+  div.classList.add("alinhamento");
+  deletar.classList.add("btnDel");
+  imgDel.style.cursor = "pointer";
+
+  //Contrapondo elementos
+  resultado.appendChild(texto)
+  deletar.appendChild(imgDel);
+  div.appendChild(deletar);
+  div.appendChild(resultado);
+  resposta1.appendChild(div);
+
+  //Evento para remover disciplina adicionada anteriormente
+  deletar.addEventListener("click", () => {
+    resultado.remove();
+    deletar.remove();
+  });
+}
+
+function addUnidCurr(){
+  alert("Unidade Curricular confirmada!");
 }
