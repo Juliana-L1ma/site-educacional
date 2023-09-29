a1be4ef05d6c831f3694082cba32c703adeb025a
Trinity <111662820+trinitydn@users.noreply.github.com>
2023-09-29 18:54:18 -0300

    Update homeAluno.php

--- homeAluno.php
+++ homeAluno.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <style> 
+    #cursos option{
+      color: #000;
+      text-align: center;
+    }
+    </style>
     <title>Home do aluno</title>
 </head>
 <body>
@@ -61,7 +67,7 @@
 
         <label class="label-curso" for="turma">Curso:</label>
 
-	<select id="cursos" name="cursos">
+	<select id="cursos" name="cursos" style="background-color: transparent; border: none; color: #fff; outline: none;">
 		<option value="Desenvolvimento de sistemas">Desenvolvimento de sistemas</option>
 		<option value="Elétrica">Elétrica</option>
 	</select>  
