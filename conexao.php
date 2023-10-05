6bb0a06f8120f1925d4e2a9685fa1cafa0f9aac6
Trinity <111662820+trinitydevs@users.noreply.github.com>
2023-10-05 11:12:18 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -7,7 +7,18 @@ $host = 'localhost';
 
 $conn= new mysqli($host, $usuario, $senha, $database);
 
+$query = "SELECT categoria FROM cursos";
+$result = mysqli_query($conn, $query);
+
 if (!$conn) {
     die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
+}
+
+// Query para selecionar a coluna "nome_curso" da tabela "cursos"
+$queryNomeCurso = "SELECT nome_curso FROM cursos";
+$resultNomeCurso = mysqli_query($conn, $queryNomeCurso);
+
+if (!$resultNomeCurso) {
+    die("Erro na consulta: " . mysqli_error($conn));
 }
 ?>
