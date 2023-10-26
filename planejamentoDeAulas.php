9df5d07e54c379f1879e56bb754f337036785968
Evelyn Victoria da Silva Santos <115710253+evelynvic23@users.noreply.github.com>
2023-10-26 10:32:46 -0300

    Add files via upload

---
+++ planejamentoDeAulas.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Planejamento de Aulas</title>
+
+  <style>
+    .botao-PDA{
+      margin: 5%;
+     
+    }
+  </style>
+</head>
+
+<body>
+  <header class="header-professor-adm">
+    <div class="div-cabecalho" id="modo-desktop">
+      <ul>
+        <a href="">
+          <li>Home</li>
+        </a>
+        <a href="">
+          <li>Perfil</li>
+        </a>
+        <a href="">
+          <li>Funcionalidades</li>
+        </a>
+        <a href="">
+          <li>Sobre o Senai</li>
+        </a>
+      </ul>
+    </div>
+    <div class="div-cabecalho" id="modo-mobile">
+      <nav id="nav-menu-hamburguer">
+        <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">
+          <span id="hamburger">
+            <img src="./img/barra-de-menu.png" alt="Menu aberto ícones criados por verry purnomo - Flaticon" />
+          </span>
+        </button>
+        <ul id="menu" role="menu">
+          <br>
+          <li><a class="header-item" href="index.html">Home</a></li><br>
+          <hr>
+          <br>
+          <li> <a class="header-item" href="products.html">Products</a></li><br>
+          <hr>
+          <br>
+          <li><a class="header-item" href="aboutus.html">About us</a></li> <br>
+          <hr>
+        </ul>
+      </nav>
+    </div>
+    <div class="div-cabecalho" id="modo-mobilee">
+      <a href="https://www.flaticon.com/br/icones-gratis/perfil" title="Perfil ícones criados por inkubators - Flaticon"
+        id="icone-perfil">
+        <img src="./img/perfil-de-usuario.png" />
+      </a>
+      <a href="https://www.flaticon.com/br/icones-gratis/engrenagem"
+        title="Engrenagem ícones criados por Freepik - Flaticon" id="icone-engrenagem"> <img
+          src="./img/engrenagem.png" />
+      </a>
+    </div>
+  </header>
+
+  <main>
+
+
+    <div class="titulo-PlanejamentoDeAula">
+      <a href="./planejamentoRegistroDeAulas-E.html"><img class="img-PlanejamentoDeAula" src="img/seta-planejamentoDeAula.png" alt=""></a>
+
+
+      <p id="titulo-PlanejamentoDeAula2">Planejamento de Aula</p>
+    </div>
+
+    <br>
+
+    <div class="meio-PDA">
+      <img class="livroPDA" src="img/livro-PDA-removebg-preview.png" alt="">
+      <p class="disciplina-PDA">Disciplinas:</p>
+    </div>
+
+
+
+
+    <?php
+    //Iniciando a conexão com o banco de dados 
+    $cx = mysqli_connect("localhost", "root", "");
+   
+    //Selecionando o banco de dados 
+    $db = mysqli_select_db($cx, "senai117_bd");
+
+
+    //Criando a query de consulta à tabela criada 
+ 
+    $alunos = mysqli_query($cx, "SELECT * FROM alunos");
+    $boletim = mysqli_query($cx, "SELECT * FROM boletim");
+    $cursos = mysqli_query($cx, "SELECT * FROM cursos");
+    $lista_alunos = mysqli_query($cx, "SELECT * FROM lista_alunos");
+    $lista_disc_prof = mysqli_query($cx, "SELECT * FROM lista_disc_prof");
+    $lista_prof_turma = mysqli_query($cx, "SELECT * FROM lista_prof_turma");
+
+
+
+
+    $lista_turma_uc = mysqli_query($cx, "SELECT * FROM lista_turma_uc");
+    $professores = mysqli_query($cx, "SELECT * FROM professores") or die( 
+      mysqli_error($cx) //Caso haja um erro na consultal, exibir erro
+      );
+      $turmas = mysqli_query($cx, "SELECT * FROM turmas");
+      $unidade_curricular = mysqli_query($cx, "SELECT * FROM unidades_curriculares");
+
+      $cursos = mysqli_query($cx, "SELECT * FROM cursos");
+
+     
+
+
+
+
+
+      ?>
+
+
+
+<div id="teste543">
+<div class="disciplinas">
+  <?php
+  while ($row = mysqli_fetch_assoc($unidade_curricular)) {
+    echo '<button class="botao-PDA" data-id="' . $row['id_unid_curricular'] . '">' . $row['nome_uc'] . '</button>';
+  }
+  ?>
+</div>
+</div>
+
+
+
+
+
+
+
+
+  </main>
+
+  <footer class="footer-professor-adm">
+
+    <div class="container">
+       
+
+        <div class="row">
+          <div class="col-6">
+            <p class="tituloRodape">Site SENAI oficial:</p><hr>
+            <p class="textoRodape">Acesse o site oficial do SENAI para ver outras informações <a href="https://www.sp.senai.br/">Clique aqui</a></p>
+          </div>
+
+          <div class="col-6">
+            <div class="contatos">
+                <p class="tituloRodape">Contatos</p><hr>
+                <p class="textoRodape">
+                   <a href="mailto:beatriz.britofer@gmail.com">Enviar email para Beatriz Brito</a><br>
+                  <a href="mailto:evelynvic23toria10@gmail.com">Enviar email para Evelyn Victoria</a><br>
+                  <a href="mailto:jp6001707@gmail.com">Enviar email para Juliana Lima</a><br>
+                  <a href="mailto:trinitynascimento@gmail.com">Enviar email para Trinity Domingues</a><br>
+                    <br>
+                </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <p class="direitosAutorais">
+                    Copyright 2023 - Beatriz Brito, Evelyn Victoria Santos, Juliana Lima e Trinity Nascimento<br>
+                    Esse site foi produzido pelas alunas citadas acima no curso de Desenvolvimento de Sistemas do Senai "Nami Jafet" para uso escolar e administrativo
+                </p>
+               
+            </div>
+          </div>
+      </div>
+
+</footer>
+
+</body>
+
+
+<script>
+  $(document).ready(function() {
+  // Quando um botão de disciplina for clicado
+  $(".btn-disciplina").click(function() {
+    // Obtenha o ID da disciplina a partir do atributo data-id
+    let disciplinaId = $(this).data("id");
+
+    // Agora você pode usar o ID da disciplina para realizar ações, como consulta ao banco de dados
+    // ou navegação para uma página relacionada a essa disciplina
+    // Exemplo de ação:
+    console.log("Disciplina selecionada (ID): " + disciplinaId);
+  });
+});
+
+</script>
+
+<script>
+  const btnMobile = document.getElementById('btn-mobile');
+
+  function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav-menu-hamburguer');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+      event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+      event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+  }
+
+  btnMobile.addEventListener('click', toggleMenu);
+  btnMobile.addEventListener('touchstart', toggleMenu);
+</script>
+
+</html>
