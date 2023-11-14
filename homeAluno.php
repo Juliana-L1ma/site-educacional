e66b6b2bd2956bcb3046f8f6cf8ce5a7b900a221
Juliana Lima <111203538+Juliana-L1ma@users.noreply.github.com>
2023-11-14 09:35:24 -0300

    Add files via upload

--- homeAluno.php
+++ homeAluno.php
@@ -1,8 +1,4 @@
-<?php
-session_start();
-//pego o nome do usuário de quem logou
-$nomeUsuario = $_SESSION["nome_usuario"];
-?>
+
 <!DOCTYPE html>
 <html lang="PT-BR">
 <head>
@@ -10,12 +6,40 @@ $nomeUsuario = $_SESSION["nome_usuario"];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <style> 
     #cursos option{
       color: #000;
       text-align: center;
     }
+    #fotoDePerfil{
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin: 0 auto;
+  padding: 5%;
+  padding-top: 5%;
+}
+.imagemAluno {
+  background-color: #0A395E;
+  width: 0px;
+  height: 0px;
+  margin-left: 40%;
+  padding: 5%;
+  padding-top: 5%;
+}
+
+.form-aluno-area{
+  margin-top: 3%; 
+  margin-left: 40%;
+}
+.btn-form-enviar{
+  padding: 1%;
+  border-radius: 5%;
+  border-color: #912B64;
+  color: #912B64;
+  --fontes-de-titulo: 'Roboto', sans-serif;
+}
     </style>
     <title>Home do aluno</title>
 </head>
@@ -60,14 +84,128 @@ $nomeUsuario = $_SESSION["nome_usuario"];
     </div>
   </header>
 
-
     <main id="mainAluno">
 
-      <div class="imagemAluno"></div>
+      <div class="imagemAluno">
+      <?php
+  session_start();
+  require_once("conexao.php");
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $targetDir = "uploadPerfil/"; // Pasta onde as imagens serão armazenadas
+  $targetFile = $targetDir . basename($_FILES["imagem"]["name"]);
+  $uploadOk = 1;
+  $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+  // Verifique se o arquivo é uma imagem real ou uma imagem falsa
+  if (isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["imagem"]["tmp_name"]);
+      if ($check !== false) {
+          echo "O arquivo é uma imagem - " . $check["mime"] . ".";
+          $uploadOk = 1;
+      } else {
+          echo "O arquivo não é uma imagem.";
+          $uploadOk = 0;
+      }
+  }
+
+  // Verifique se o arquivo já existe
+  if (file_exists($targetFile)) {
+      // echo " Desculpe, o arquivo já existe. ";
+      $uploadOk = 0;
+  }
+
+  // Verifique o tamanho máximo do arquivo (ajuste para o tamanho desejado)
+  if ($_FILES["imagem"]["size"] > 500000) {
+      // echo "Desculpe, o arquivo é muito grande.";
+      $uploadOk = 0;
+  }
+
+  // Verifique se $uploadOk está definido como 0 por algum erro
+  if ($uploadOk == 0) {
+      // echo " Desculpe, seu arquivo não foi enviado. "
+  } else {
+    if ($_SESSION['tipo_usuario'] === 'alunos' && isset($_SESSION['num_matricula_aluno'])) {
+      $id_usuario = $_SESSION['num_matricula_aluno'];
+  
+   // Consulta para obter a imagem anterior do aluno
+   $query = "SELECT fotoDoAluno FROM alunos WHERE num_matricula_aluno = $id_usuario";
+   $result = mysqli_query($conn, $query);
+
+   if ($result && mysqli_num_rows($result) > 0) {
+       $row = mysqli_fetch_assoc($result);
+       $imagem_anterior = $row['fotoDoAluno'];
+
+       // Verifique se existe uma imagem anterior
+       if (!empty($imagem_anterior)) {
+           // Exclua a imagem anterior do servidor
+           if (file_exists($imagem_anterior)) {
+               unlink($imagem_anterior);
+           }
+       }
+   }
+
+      if (move_uploaded_file($_FILES["imagem"]["tmp_name"], $targetFile)) {
+          // echo "O arquivo " . htmlspecialchars(basename($_FILES["imagem"]["name"])) . " foi enviado com sucesso.";
+  
+          // Atualize o banco de dados com o caminho da imagem do aluno
+          $query = "UPDATE alunos SET fotoDoAluno = '$targetFile' WHERE num_matricula_aluno = $id_usuario";
+          if (mysqli_query($conn, $query)) {
+              // echo "Banco de dados atualizado com sucesso.";
+          } else {
+              // echo "Erro ao atualizar o banco de dados: " . mysqli_error($conn);
+          }
+      } else {
+          // echo "Desculpe, houve um erro ao enviar o arquivo.";
+      }
+  }
+
+  }
+}
+
+if ($_SESSION['tipo_usuario'] === 'alunos' && isset($_SESSION['num_matricula_aluno'])) {
+  $id_usuario = $_SESSION['num_matricula_aluno'];
+  $query = "SELECT fotoDoAluno FROM alunos WHERE num_matricula_aluno = $id_usuario";
+  $result = mysqli_query($conn, $query);
+
+  if ($result && mysqli_num_rows($result) > 0) {
+      $row = mysqli_fetch_assoc($result);
+      $caminho_imagem = $row['fotoDoAluno'];
+
+      if (!empty($caminho_imagem)) {
+        echo '<img src="' . $caminho_imagem . '" id="fotoDePerfil"  alt="Imagem do Aluno">';
+    } else {
+        // echo 'Nenhuma imagem de perfil encontrada.';
+    }
+  }
+}
+else {
+echo "nao foi";
+}
+?>
+
+</div>
+
+
+
+<div class="form-aluno-area">
+
+  <form id="formImagem" action="homeAlunoTeste.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="imagem">
+    <input type="button" id="enviarImagemBtn" value="Enviar Imagem">
+  </form>
+
+  <img id="editarBtn" src="./img/lapis-edicao.png" alt="Editar Imagem" style="cursor: pointer;">
+</div>
+
+   
       <div class="informacoesDoAluno">
 
-        <p id="nomeDoAluno"><?php echo $nomeUsuario;?></p>
+      
+      <p id="nomeDoAluno"><?php echo $_SESSION['nome_aluno']; ?></p>
+
+
+
         <hr id="hr-aluno"><br>
 
         <label class="label-curso" for="turma">Curso:</label>
@@ -77,12 +215,12 @@ $nomeUsuario = $_SESSION["nome_usuario"];
 		<option value="Elétrica">Elétrica</option>
 	</select>  
 
-  <p id="nomeDaTurma"> <br>$nomeDaTurma</p>
+  <p id="nomeDaTurma"><br>$nomeDaTurma</p>
       </div>
 
       <div id="caixa-botoes">
 
-        <a href="./meuBoletim.php" class="buttonDoAluno">Boletim</a>
+        <a href="./meuBoletim.html" class="buttonDoAluno">Boletim</a>
         <a href="./" class="buttonDoAluno">Informações</a>
 
       </div>
@@ -128,6 +266,8 @@ $nomeUsuario = $_SESSION["nome_usuario"];
     
     </footer>
 </body>
+
+
 <script>
   const btnMobile = document.getElementById('btn-mobile');
 
@@ -146,5 +286,21 @@ if (active) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+//script do botão de editar
+document.addEventListener('DOMContentLoaded', function () {
+        var formImagem = document.getElementById('formImagem');
+        formImagem.style.display = 'none';
+
+        var enviarImagemBtn = document.getElementById('enviarImagemBtn');
+        enviarImagemBtn.addEventListener('click', function () {
+            formImagem.submit(); // Submeter o formulário manualmente
+        });
+
+        document.getElementById('editarBtn').addEventListener('click', function () {
+            formImagem.style.display = (formImagem.style.display === 'none' || formImagem.style.display === '') ? 'block' : 'none';
+        });
+    });
 </script>
+
 </html>
--- processa_login.php
+++ processa_login.php
@@ -9,26 +9,57 @@ $senha = $_POST['senha'];
 $query = "SELECT num_matricula_aluno FROM alunos WHERE email_educacional_aluno = '$email' AND senha_educacional_aluno = '$senha'";
 $resultAluno = mysqli_query($conn, $query);
 
+ // ...
 if ($resultAluno && mysqli_num_rows($resultAluno) > 0) {
+
     // O usuário é um aluno
     $_SESSION['tipo_usuario'] = 'alunos';
     $_SESSION["email"] = $email;
     $_SESSION["senha"] = $senha;
     $usuario = mysqli_query($conn, "SELECT * FROM alunos WHERE email_educacional_aluno = '$email' AND senha_educacional_aluno = '$senha'");
-
+    
     while($linha = mysqli_fetch_assoc($usuario)){
+        $_SESSION['num_matricula_aluno'] = $linha['num_matricula_aluno'];
+        $_SESSION['nome_aluno'] = $linha['nome_aluno'];
         $_SESSION["id_usuario"] = $linha["num_matricula_aluno"];
         $_SESSION["nome_usuario"] = $linha["nome_aluno"];
         $_SESSION["sobrenome_usuario"] = $linha["sobrenome_aluno"];
     }
-
-    header("Location: homeAluno.php");
+    
+    header("Location: homeAlunoTeste.php");
     exit();
 }
+// ...
+
+
+
+
+//verifica se o aluno está logado e faz as ações de pegar imagem 
+if ($_SESSION['tipo_usuario'] === 'alunos') {
+    $id_usuario = $_SESSION['num_matricula_aluno'];
+
+    // Verifica se uma imagem foi enviada
+    if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
+        $nome_temporario = $_FILES['imagem']['tmp_name'];
+        $caminho_destino = 'uploadPerfil' . $id_usuario . '.jpeg'; // Personalize o caminho e o nome do arquivo conforme necessário
+
+        // Move a imagem para o destino
+        if (move_uploaded_file($nome_temporario, $caminho_destino)) {
+            // Atualiza o banco de dados com o caminho da imagem
+            $query = "UPDATE alunos SET fotoDoAluno = '$caminho_destino' WHERE num_matricula_aluno = $id_usuario";
+            mysqli_query($conn, $query);
+        }
+    }
+
+
+    header("Location: homeAlunoTeste.php");
+}
+
 
 // Verifica na tabela "administrador"
 $query = "SELECT id_adm FROM administrador WHERE email_administrativo = '$email' AND senha_administrativa = '$senha'";
 $resultAdm = mysqli_query($conn, $query);
+
 
 if ($resultAdm && mysqli_num_rows($resultAdm) > 0) {
     // O usuário é um administrador
@@ -45,6 +76,8 @@ if ($resultAdm && mysqli_num_rows($resultAdm) > 0) {
     exit();
 }
 
+
+
 // Verifica na tabela "professores"
 $query = "SELECT nif_professor FROM professores WHERE email_educacional_professor = '$email' AND senha_educacional_professor = '$senha'";
 $resultProf = mysqli_query($conn, $query);
@@ -60,7 +93,7 @@ if ($resultProf && mysqli_num_rows($resultProf) > 0) {
             $_SESSION["id_usuario"] = $linha["nif_professor"];
             $_SESSION["nome_usuario"] = $linha["nome_professor"];
         }
-    
+
     header("Location: homeProfessor.php");
     exit();
 }
