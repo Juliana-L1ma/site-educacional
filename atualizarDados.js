55cc2ae4101045e6e084535cb0ef6bc9c8df0268
Trinity <111662820+trinitydevs@users.noreply.github.com>
2023-11-14 22:06:00 -0300

    Update atualizarDados.js

--- atualizarDados.js
+++ atualizarDados.js
@@ -1,165 +1,63 @@
 //ATUALIZANDO DADOS DA TABELA ALUNOS
-var attNomeAluno = document.getElementsByClassName("attNomeAluno");
-var attSobrenomeAluno = document.getElementsByClassName("attSobAluno");
-var attRgAluno = document.getElementsByClassName("attRgAluno");
-var attCpfAluno = document.getElementsByClassName("attCpfAluno");
-
-//ATUALIZA NOME ALUNO
-for (var i = 0; i < attNomeAluno.length; i++) {
-  // Adiciona um evento de clique a cada elemento
-  attNomeAluno[i].addEventListener("click", function (event) {
-    // Armazena a referência do elemento clicado para acesso posterior
-    var elementoClicado = this;
-
-    Swal.fire({
-      title: "Atualizar",
-      text: "Escreva no campo para alterar",
-      input: "text",
-      showCancelButton: true,
-    }).then((result) => {
-      if (result.value) {
-        // Faz a solicitação AJAX para o PHP
-        $.ajax({
-          url: "atualizar.php", // Substitua pelo caminho correto do seu arquivo PHP
-          method: "POST",
-          data: {
-            coluna: "nome_aluno", // Coluna que será atualizada
-            valor: result.value, // Novo valor digitado
-            matricula: elementoClicado.parentElement.children[0].innerText, // Matrícula do aluno
-          },
-          success: function (response) {
-            // Exibe uma mensagem ou realiza outras ações após a atualização
-            console.log(response);
-            Swal.fire({
-              title: "Dado atualizado com sucesso",
-              type: "success",
-            });
-            setTimeout(function () {
-              location.reload();
-            }, 2000);
-          },
-          error: function (error) {
-            console.error(error);
-          },
-        });
-      }
-    });
+function atualizarDadoAluno(coluna, elementoClicado) {
+  Swal.fire({
+    title: "Atualizar",
+    text: "Escreva no campo para alterar",
+    input: "text",
+    showCancelButton: true,
+  }).then((result) => {
+    if (result.value) {
+      $.ajax({
+        url: "atualizar.php",
+        method: "POST",
+        data: {
+          coluna: coluna,
+          valor: result.value,
+          matricula: elementoClicado.parentElement.children[0].innerText,
+        },
+        success: function (response) {
+          console.log(response);
+          Swal.fire({
+            title: "Dado atualizado com sucesso",
+            type: "success",
+          });
+          setTimeout(function () {
+            location.reload();
+          }, 2000);
+        },
+        error: function (error) {
+          console.error(error);
+        },
+      });
+    }
   });
 }
 
-//ATUALIZA SOBRENOME ALUNO
-for (var i = 0; i < attSobrenomeAluno.length; i++) {
-  attSobrenomeAluno[i].addEventListener("click", function (event) {
-    var elementoClicado = this;
-
-    Swal.fire({
-      title: "Atualizar",
-      text: "Escreva no campo para alterar",
-      input: "text",
-      showCancelButton: true,
-    }).then((result) => {
-      if (result.value) {
-        $.ajax({
-          url: "atualizar.php",
-          method: "POST",
-          data: {
-            coluna: "sobrenome_aluno",
-            valor: result.value,
-            matricula: elementoClicado.parentElement.children[0].innerText,
-          },
-          success: function (response) {
-            console.log(response);
-            Swal.fire({
-              title: "Dado atualizado com sucesso",
-              type: "success",
-            });
-            setTimeout(function () {
-              location.reload();
-            }, 2000);
-          },
-          error: function (error) {
-            console.error(error);
-          },
-        });
-      }
+function atualizarDados(elementos, coluna) {
+  for (var i = 0; i < elementos.length; i++) {
+    elementos[i].addEventListener("click", function (event) {
+      var elementoClicado = this;
+      atualizarDadoAluno(coluna, elementoClicado);
     });
-  });
+  }
 }
 
-//ATUALIZA RG ALUNO
-for (var i = 0; i < attRgAluno.length; i++) {
-    attRgAluno[i].addEventListener("click", function (event) {
-    var elementoClicado = this;
+var elementosParaAtualizar = [
+  { elementos: document.getElementsByClassName("attNomeAluno"), coluna: "nome_aluno" },
+  { elementos: document.getElementsByClassName("attSobAluno"), coluna: "sobrenome_aluno" },
+  { elementos: document.getElementsByClassName("attRgAluno"), coluna: "rg_aluno" },
+  { elementos: document.getElementsByClassName("attCpfAluno"), coluna: "cpf_aluno" },
+  { elementos: document.getElementsByClassName("attNascAluno"), coluna: "data_nascimento_aluno" },
+  { elementos: document.getElementsByClassName("attTelAluno"), coluna: "telefone_aluno" },
+  { elementos: document.getElementsByClassName("attEndAluno"), coluna: "endereco_aluno" },
+  { elementos: document.getElementsByClassName("attNumAluno"), coluna: "endereco_numero_aluno" },
+  { elementos: document.getElementsByClassName("attCompAluno"), coluna: "complemento_end_aluno" },
+  { elementos: document.getElementsByClassName("attEmailAluno"), coluna: "email_pessoal_aluno" },
+  { elementos: document.getElementsByClassName("attEmailEdAluno"), coluna: "email_educacional_aluno" },
+  { elementos: document.getElementsByClassName("attSenhaEdAluno"), coluna: "senha_educacional_aluno" },
+  { elementos: document.getElementsByClassName("attNomeProfessor"), coluna: "nome_professor"}
+];
 
-    Swal.fire({
-      title: "Atualizar",
-      text: "Escreva no campo para alterar",
-      input: "text",
-      showCancelButton: true,
-    }).then((result) => {
-      if (result.value) {
-        $.ajax({
-          url: "atualizar.php",
-          method: "POST",
-          data: {
-            coluna: "rg_aluno",
-            valor: result.value,
-            matricula: elementoClicado.parentElement.children[0].innerText,
-          },
-          success: function (response) {
-            console.log(response);
-            Swal.fire({
-              title: "Dado atualizado com sucesso",
-              type: "success",
-            });
-            setTimeout(function () {
-              location.reload();
-            }, 2000);
-          },
-          error: function (error) {
-            console.error(error);
-          },
-        });
-      }
-    });
-  });
-}
-
-//ATUALIZA CPF ALUNO
-for (var i = 0; i < attCpfAluno.length; i++) {
-    attCpfAluno[i].addEventListener("click", function (event) {
-    var elementoClicado = this;
-
-    Swal.fire({
-      title: "Atualizar",
-      text: "Escreva no campo para alterar",
-      input: "text",
-      showCancelButton: true,
-    }).then((result) => {
-      if (result.value) {
-        $.ajax({
-          url: "atualizar.php",
-          method: "POST",
-          data: {
-            coluna: "cpf_aluno",
-            valor: result.value,
-            matricula: elementoClicado.parentElement.children[0].innerText,
-          },
-          success: function (response) {
-            console.log(response);
-            Swal.fire({
-              title: "Dado atualizado com sucesso",
-              type: "success",
-            });
-            setTimeout(function () {
-              location.reload();
-            }, 2000);
-          },
-          error: function (error) {
-            console.error(error);
-          },
-        });
-      }
-    });
-  });
-}
+elementosParaAtualizar.forEach(item => {
+  atualizarDados(item.elementos, item.coluna);
+});
